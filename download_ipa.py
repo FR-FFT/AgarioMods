031a53a1d9932d1d7d26e20b8862b5658c8421aa
FR-FFT <surmanm@williamperkin.org.uk>
2024-12-21 13:39:38 +0000

    recommit all files

---
+++ download_ipa.py
@@ -0,0 +1,49 @@
+import asyncio
+import os
+import signal
+from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+
+TARGET_BOT_USERNAME = "eeveedecrypterbot"
+APP_STORE_LINK = "https://apps.apple.com/app/id995999703"
+
+
+SESSION_FILE = "user_session"  # encrypted session file
+
+async def handle_message(client, message):
+    """Handle incoming messages from the target bot."""
+    if message.chat.username == TARGET_BOT_USERNAME and message.document:
+        print(f"Document received: {message.document.file_name}")
+        await message.download("Agario.ipa")
+        print("File downloaded!")
+        client._should_stop.set()
+
+async def main():
+    """Main function to send a message and wait for the response."""
+    app = Client(SESSION_FILE)
+
+    # Add the message handler
+    app.add_handler(MessageHandler(handle_message, filters=filters.document))
+
+    # Start the client
+    await app.start()
+
+    app._should_stop = asyncio.Event()
+
+    # Send the App Store link to the bot
+    print(f"Sending message to {TARGET_BOT_USERNAME}...")
+    await app.send_message(f"@{TARGET_BOT_USERNAME}", APP_STORE_LINK)
+    print("Message sent. Waiting for the response...")
+
+    # Keep the client running to process messages
+    try:
+        await asyncio.wait_for(app._should_stop.wait(), timeout=10 * 60)
+    except asyncio.TimeoutError:
+        print("Timed out after 10 minutes. Killing the task.")
+        os.kill(os.getpid(), signal.SIGINT)
+    finally:
+        await app.stop()
+        print("Client stopped. Exiting.")
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ inject_mods.py
@@ -0,0 +1,135 @@
+UNPACKEDIPA_PATH = "downloads/Agario"
+
+import os
+import subprocess
+import shutil
+import random
+
+"""
+usage: pyzule [-h] [-i input] [-o output] [-z .pyzule] [-n name] [-v version] [-b bundle id] [-m minimum] [-c [level]]
+              [-k icon] [-x entitlements] [-l plist] [-r url [url ...]] [-f files [files ...]] [-u] [-w] [-d] [-s]
+              [-e] [-g] [-p] [-t] [--update]
+
+an azule "clone" written in python3.
+
+options:
+  -h, --help            show this help message and exit
+  -i input              the .ipa/.app to patch
+  -o output             the name of the patched .ipa/.app that will be created
+  -z .pyzule            the .pyzule file to get info from
+  -n name               modify the app's name
+  -v version            modify the app's version
+  -b bundle id          modify the app's bundle id
+  -m minimum            change MinimumOSVersion
+  -c [level]            the compression level of the output ipa (default is 6, 0-9)
+  -k icon               an image file to use as the app icon
+  -x entitlements       a file containing entitlements to sign the app with
+  -l plist              a plist to merge with the existing Info.plist
+  -r url [url ...]      url schemes to add
+  -f files [files ...]  tweak files to inject into the ipa
+  -u                    remove UISupportedDevices
+  -w                    remove watch app
+  -d                    enable files access
+  -s                    fakesigns the ipa (for use with appsync)
+  -e                    remove app extensions
+  -g                    remove encrypted extensions
+  -p                    inject into @executable_path
+  -t                    use substitute instead of substrate
+  --update              check for updates
+  """
+def inject_images(mod_type, working_path, folder=None):
+    if folder is not None:
+        if not os.path.exists(f"{working_path}/Payload/agar.io.app/{folder}"):
+            os.mkdir(f"{working_path}/Payload/agar.io.app/{folder}")
+            src_string = "mods/images/{mod_type}/"+folder+"/{item}"
+    else:
+        src_string = "mods/images/{mod_type}/{item}"
+        
+    for item in os.listdir(f"mods/images/{mod_type}"):
+        src = src_string.format(mod_type=mod_type, item=item)
+        dst = f"{working_path}/Payload/agar.io.app/{item}"
+        shutil.copyfile(src, dst)
+
+def inject_other(mod_type, working_path):
+    for item in os.listdir(f"mods/other/{mod_type}"):
+        src = f"mods/other/{mod_type}/{item}"
+        dst = f"{working_path}/Payload/agar.io.app/{item}"
+        shutil.copyfile(src, dst)
+
+def inject_tweaks(name, new_unpacked_ipa_path, tweaks):
+    cmd = ["pyzule", "-uwdeg", "-i", f"{name}.ipa", "-o", f"{new_unpacked_ipa_path}-patched", "-f"] + tweaks
+    subprocess.run(cmd)
+
+
+def prepare_files(base_unpacked_ipa_path, type):
+    if not os.path.exists(f"working"):
+        os.mkdir("working")
+    new_unpacked_ipa_path = f"working/{type}/Agario"
+    if os.path.exists(new_unpacked_ipa_path):
+        shutil.rmtree(new_unpacked_ipa_path) # clean up
+    
+    print("Copying unpacked ipa to working directory")
+    shutil.copytree(base_unpacked_ipa_path, new_unpacked_ipa_path) # just to prevent collision
+    
+
+
+def inject_cracked_kahraba_mod(base_unpacked_ipa_path):
+    print("Creating cracked kahraba mod")
+    
+    prepare_files(base_unpacked_ipa_path, "kahraba")
+
+    new_unpacked_ipa_path = "working/kahraba/Agario"
+
+    # copy necessary files into the payload
+    print("Injecting files necessary for the mod to function")
+    inject_images("kahraba", new_unpacked_ipa_path)
+    inject_other("kahraba", new_unpacked_ipa_path)
+
+    print("Repacking ipa")
+    shutil.make_archive("Kahraba cracked", "zip", new_unpacked_ipa_path)
+    os.rename( "Kahraba cracked.zip", "Kahraba cracked.ipa")
+
+    inject_tweaks("Kahraba cracked", new_unpacked_ipa_path, ["mods/tweaks/kahraba/AgarioTweak.dylib", "mods/tweaks/kahraba/kahraba.dylib"])
+    shutil.rmtree("working")
+
+def inject_xelahot_mod(base_unpacked_ipa_path):
+    print("Creating xelahot's mod")
+    
+    prepare_files(base_unpacked_ipa_path, "xelahot")
+
+    new_unpacked_ipa_path = "working/xelahot/Agario"
+
+    # copy necessary files into the payload
+    print("Injecting files necessary for the mod to function")
+    inject_images("xelahot", new_unpacked_ipa_path, folder="xelahotimages")
+
+    print("Repacking ipa")
+    shutil.make_archive("Xelahot", "zip", new_unpacked_ipa_path)
+    os.rename( "Xelahot.zip", "Xelahot.ipa")
+
+    inject_tweaks("Xelahot", new_unpacked_ipa_path, ["mods/tweaks/xelahot/xelahot.deb", "mods/tweaks/xelahot/images.deb"])
+
+    shutil.rmtree("working")
+    
+
+def inject_ctrl_mod(base_unpacked_ipa_path):
+    print("Creating QxAnarky's ctrl mod")
+    
+    prepare_files(base_unpacked_ipa_path, "qxanarky")
+
+    new_unpacked_ipa_path = "working/ctrl/Agario"
+
+    # copy necessary files into the payload
+    print("Injecting files necessary for the mod to function")
+    inject_images("qxanarky", new_unpacked_ipa_path, folder="qxanarkyimages")
+
+    print("Repacking ipa")
+    shutil.make_archive("Ctrl", "zip", new_unpacked_ipa_path)
+    os.rename( "Ctrl.zip", "Ctrl.ipa")
+
+    inject_tweaks("Ctrl", new_unpacked_ipa_path, ["mods/tweaks/qxanarky/ctrl.dylib", "mods/tweaks/xelahot/images.deb"])
+
+    shutil.rmtree("working")
+
+inject_cracked_kahraba_mod("downloads/Agario") # test
+
