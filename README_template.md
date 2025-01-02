b97e51b224116796ae85edef37c5afb312ce852f
FR-FFT <surmanm@williamperkin.org.uk>
2025-01-02 14:53:04 +0000

    Add repos, readme, re enable daily update

---
+++ README_template.md
@@ -0,0 +1,53 @@
+<!-- README.md is generated via README_template.md. Do not modify README.md manually. -->
+<h1 align="center">Agario mods</h1>
+
+<p align="center"><strong>A collection of Agar.io mods, for easy sideloading with eSign, Appdb, Scarlet, AltStore, Feather. Updates automatically once a day. Last updated: {update_date}, Agar.io version {version}</strong></p>
+<p align="center">If you want to learn how to sideload Agar.io mods (for free, with no revokes), try https://sideloadling.vercel.app</p>
+
+<p align="center">
+    <a href="https://github.com/FR-FFT/AgarioMods/releases/latest"><img src="https://img.shields.io/github/downloads/TestAccountFooBar/AgarMod/latest/total?color=%23007BFF&label=Downloads%20(Latest%20Release)" alt="Downloads (Latest Release) Badge"></a>
+    <a href="https://github.com/FR-FFT/AgarioMods/releases"><img src="https://img.shields.io/github/downloads/TestAccountFooBar/AgarMod/total?color=%23007BFF&label=Total%20Downloads" alt="Total Downloads Badge"></a>
+</p>
+
+<p align="center">
+    <a href="https://fwuf.in/#/esign://addsource?url=https://raw.githubusercontent.com/FR-FFT/AgarioMods/main/esign_repo.json">
+    <img src="https://img.shields.io/badge/Add%20repo%20to%20ESign-%20blue?style=for-the-badge&color=1e90ff" alt="Add repo to ESign">
+  </a>
+</p>
+
+<p align="center">
+    <a href="https://appdb.to/repos/import?url=https://raw.githubusercontent.com/TestAccountFooBar/AgarMod/main/esign_repo.json">
+    <img src="https://img.shields.io/badge/Add%20repo%20to%20Appdb-%20blue?style=for-the-badge&color=0048ba" alt="Add repo to Appdb">
+  </a>
+</p>
+
+<p align="center">
+    <a href="https://fwuf.in/#/scarlet://repo=https://raw.githubusercontent.com/TestAccountFooBar/AgarMod/main/scarlet_repo.json">
+    <img src="https://img.shields.io/badge/Add%20repo%20to%20Scarlet-%20red?style=for-the-badge&color=ff0000" alt="Add repo to Scarlet">
+  </a>
+</p>
+
+<p align="center">
+    <a href="https://fwuf.in/#/altstore://source?url=https://raw.githubusercontent.com/TestAccountFooBar/AgarMod/main/esign_repo.json">
+    <img src="https://img.shields.io/badge/Add%20repo%20to%20AltStore-%202E7E85?style=for-the-badge&color=2E7E85" alt="Add repo to AltStore">
+  </a>
+</p>
+
+<p align="center">
+    <a href="https://fwuf.in/#/feather://source/https://raw.githubusercontent.com/TestAccountFooBar/AgarMod/main/esign_repo.json">
+    <img src="https://img.shields.io/badge/Add%20repo%20to%20Feather-%20blue?style=for-the-badge&color=808BF8" alt="Add repo to Feather">
+  </a>
+</p>
+
+
+<!-- <p align="center">
+    <a href="https://fwuf.in/#/sideloadly:https://github.com/TestAccountFooBar/AgarMod/releases/download/{version}/AgarMod_{version}.ipa">
+    <img src="https://img.shields.io/badge/Sideload%20with%20Sideloadly-%2016CDC4?style=for-the-badge&color=16CDC4" alt="Sideload with Sideloadly">
+  </a>
+</p> -->
+
+## Mod List
+
+| Mod Name | Download Link |
+|----------|---------------|
+{modlist}
--- upload_mods.py
+++ upload_mods.py
@@ -1,8 +1,73 @@
 import os
 import sys
 from github import Github
+import requests
+import datetime
 
-def upload_assets(repo_name, token, tag_name, release_name, body, folder):
+def fetch_version():
+    # TODO: error handling / retries
+    url = "https://raw.githubusercontent.com/FR-FFT/AgarioMods/refs/heads/main/version.txt"
+    response = requests.get(url)
+    return response.text.strip()
+
+def get_current_date():
+    return datetime.datetime.now().strftime("%Y-%m-%d")
+
+def construct_scarlet_repo_txt(asset_upload_urls, version):
+    repo_name = "FR-FFT's Agar.io Mod collection"
+    # repo_icon = "https://avatars.githubusercontent.com/u/136937878?v=4"
+    json = {
+        "META": {
+            "repoName": repo_name,
+            # "repoIcon": repo_icon,
+        },
+        "Agar.io mods": [{
+            "name": "Agar.io",
+            "version": version,
+            "down": asset_upload_url,
+            "dev": "",
+            "category": "Agar.io Mods",
+            "description": f"Agar.io mods for version {version}",
+            "bundleID": "com.miniclip.agar.io",
+            "appstore": "com.miniclip.agar.io",
+            "contact": {
+                "web": "",
+                "PayPal": ""
+            }
+        } for asset_upload_url in asset_upload_urls]
+    }
+
+def construct_esign_repo_txt(asset_upload_urls, version):
+
+    repo_name = "FR-FFT's Agar.io Mod collection"
+    # repo_icon = "https://avatars.githubusercontent.com/u/136937878?v=4"
+    json = {
+        "name": repo_name,
+        "identifier": "fr-fft.github.io",
+        # "iconURL": repo_icon,
+        "website": "https://github.com/FR-FFT/AgarioMods",
+        "sourceURL": "https://raw.githubusercontent.com/FR-FFT/AgarioMods/refs/heads/main/esign_repo.json",
+        "apps": [
+            {
+                "name": "Agar.io",
+                "bundleIdentifier": "com.miniclip.agar.io",
+                "developerName": "",
+                "version": version,
+                "versionDate": get_current_date(),
+                "downloadURL": asset_upload_url,
+                "localizedDescription": f"Agar.io mod for version {version}",
+                "iconURL": "",
+                "tintColor": "FF0000",
+                "size": 40000000,
+                "screenshotURLs": [
+                    
+                ]
+                }
+            for asset_upload_url in asset_upload_urls
+        ]
+    }
+
+def upload_assets_and_update_files(repo_name, token, tag_name, release_name, body, folder):
     # Authenticate with GitHub
     g = Github(token)
     repo = g.get_repo(repo_name)
@@ -23,30 +88,61 @@ def upload_assets(repo_name, token, tag_name, release_name, body, folder):
         print(f"Created release {tag_name}.")
 
     # Upload assets
+    asset_upload_urls = []
     for file_name in os.listdir(folder):
         if file_name.endswith(".ipa"):
             file_path = os.path.join(folder, file_name)
             print(f"Uploading {file_name}...")
             try:
-                release.upload_asset(
+                asset =release.upload_asset(
                     path=file_path,
                     name=file_name,
                     content_type="application/octet-stream",
                 )
+                asset_upload_urls.append(asset.browser_download_url)
                 print(f"Uploaded {file_name}.")
             except Exception as e:
                 print(f"Failed to upload {file_name}: {e}")
 
+
+
+    scarlet_repo_txt = construct_scarlet_repo_txt(asset_upload_urls, version)
+    try:
+        repo.get_contents("scarlet_repo.json", ref="main")
+        repo.update_file("scarlet_repo.json", "Updated Scarlet repo", scarlet_repo_txt, branch="main")
+    except:
+        repo.create_file("scarlet_repo.json", "Created Scarlet repo", scarlet_repo_txt, branch="main")
+
+    esign_repo_txt = construct_esign_repo_txt(asset_upload_urls, version)
+    try:
+        repo.get_contents("esign_repo.json", ref="main")
+        repo.update_file("esign_repo.json", "Updated eSign repo", esign_repo_txt, branch="main")
+    except:
+        repo.create_file("esign_repo.json", "Created eSign repo", esign_repo_txt, branch="main")
+
+
+    # Update README.md
+    with open("README_template.md", "r") as f:
+        readme_template = f.read()
+    modlist="\n".join([f"| {asset_upload_url.split("/")[-1].replace(".ipa", "").replace(".", " ")} | {asset_upload_url} |" for asset_upload_url in asset_upload_urls])
+
+    try:
+        repo.get_contents("README.md", ref="main")
+        repo.update_file("README.md", "Updated README", readme_template.format(version=version, update_date=get_current_date(), modlist=modlist), branch="main")
+    except:
+        repo.create_file("README.md", "Created README", readme_template.format(version=version, update_date=get_current_date(), modlist=modlist), branch="main")
+
+
 if __name__ == "__main__":
-    if len(sys.argv) < 6:
-        print("Usage: python upload_mods.py <repo_name> <token> <tag_name> <release_name> <body> <folder>")
+    if len(sys.argv) < 1:
+        print("Usage: python upload_mods.py <token>")
         sys.exit(1)
+    version = fetch_version()
+    repo_name = os.environ['GITHUB_REPOSITORY']
+    token = sys.argv[1]
+    tag_name = f"v{version}"
+    release_name = f"Agar.io Mods v{version}"
+    body = f"Mods for Agar.io version {version}"
+    folder = "./ModifiedIPAs"
 
-    repo_name = sys.argv[1]
-    token = sys.argv[2]
-    tag_name = sys.argv[3]
-    release_name = sys.argv[4]
-    body = sys.argv[5]
-    folder = sys.argv[6]
-
-    upload_assets(repo_name, token, tag_name, release_name, body, folder)
+    upload_assets_and_update_files(repo_name, token, tag_name, release_name, body, folder)
